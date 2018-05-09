f56b84b07f002838c48405bf1c1f96f0d760332e
sharry4 <sharrytong4@gmail.com>
2018-05-09 19:30:36 -0400

    end-of-semester upload

---
+++ class-demo/greensock/js/main.js
@@ -0,0 +1,11 @@
+
+// TweenMax.to("#pooch", 2, {left:600});
+ // TweenMax.to("#pooch", 6, {x:600, ease:Elastic.easeOut});
+ TweenLite.to(graph, 1, { ease: SlowMo.ease.config(0.7, 0.7, false), y: -500 });
+// TweenMax.to("#pooch", 2, {x:600, rotation:'360', scale: 0.5});
+
+// var tl = new TimelineLite();
+// tl.to("#pooch", 1, {x:500,y:0,scale: 0.5})
+// .to("#pooch", 1, {x:500,y:500,scale: 1})
+// .to("#pooch", 1, {x:0,y:500, scale: 0.5})
+// .to("#pooch", 1, {x:0,y:0,scale: 1});
